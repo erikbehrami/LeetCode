17d4bb40b1bd827cb709dc5c2da75e928160ebab
erikbehrami <154816038+erikbehrami@users.noreply.github.com>
2024-01-10 19:10:52 +0100

    Time: 2 ms (44.72%) | Memory: 6.6 MB (15.90%) - LeetSync

---
+++ 2491-smallest-even-multiple/smallest-even-multiple.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int smallestEvenMultiple(int n) {
+    int rezultati;
+    if (n % 2 == 0) {
+        return rezultati = n;
+         }
+          else {
+        return rezultati = 2 * n;
+        }
+    }
+  };
